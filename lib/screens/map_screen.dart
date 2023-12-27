3f79e5971af57a02df4f6aa8628b82b7fb022e19
SugawaraTomo <si20026@edu.aomori-u.ac.jp>
2023-12-27 15:20:21 +0900

    位置情報　通知　組み込み　まで

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -14,7 +15,11 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
   late GoogleMapController mapController;
+
   //   現在地を監視するためのstream
   late StreamSubscription<Position> positionStream;
   Set<Marker> markers = {};
@@ -29,6 +34,7 @@ class _MapScreenState extends State<MapScreen> {
     accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
     distanceFilter: 0,
   );
+
   @override
   void dispose() {
     mapController.dispose();
@@ -40,60 +46,114 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: initialCameraPosition,
-        onMapCreated: (GoogleMapController controller) async {
-          mapController = controller;
-          await _requestPermission();
-          await _moveToCurrentLocation();
-          _watchCurrentLocation();
-        },
-        onLongPress: (LatLng latLang) {
-          setState(() {
-            markers.add(Marker(
-              markerId: const MarkerId('tapped_location'),
-              position: LatLng(
-                latLang.latitude,
-                latLang.longitude,
-              ),
-            ));
-          });
-        },
-        myLocationButtonEnabled: false,
-        markers: markers,
+        body: Column(children: <Widget>[
+      Container(
+        width: double.infinity,
+        height: 700,
+        child: GoogleMap(
+          initialCameraPosition: initialCameraPosition,
+          onMapCreated: (GoogleMapController controller) async {
+            mapController = controller;
+            await _requestPermission();
+            await _moveToCurrentLocation();
+            _watchCurrentLocation();
+          },
+          onLongPress: (LatLng latLang) {
+            setState(() {
+              markers.add(Marker(
+                markerId: const MarkerId('tapped_location'),
+                position: LatLng(
+                  latLang.latitude,
+                  latLang.longitude,
+                ),
+              ));
+            });
+          },
+          myLocationButtonEnabled: false,
+          markers: markers,
+        ),
       ),
-    );
+      Center(
+        child: OutlinedButton(
+          onPressed: () {}, //_showNotification,
+          child: Text('通知を表示'),
+        ),
+      ),
+    ]));
   }
 
+  @override
+  void initState() {
+    super.initState();
+
+    _initializePlatformsSpecifics();
+  }
+
+  void _initializePlatformsSpecifics() {
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    var initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveBackgroundNotificationResponse: (NotificationResponse res) {
+      debugPrint('payload:${res.payload}');
+    });
+  }
 
   void _watchCurrentLocation() {
     // 現在地を監視
     positionStream =
         Geolocator.getPositionStream(locationSettings: locationSettings)
             .listen((position) async {
-          // マーカーの位置を更新
-          setState(() {
-            markers.removeWhere(
-                    (marker) => marker.markerId == const MarkerId('current_location'));
+      // マーカーの位置を更新
+      setState(() {
+        markers.removeWhere(
+            (marker) => marker.markerId == const MarkerId('current_location'));
 
-            markers.add(Marker(
-              markerId: const MarkerId('current_location'),
-              position: LatLng(
-                position.latitude,
-                position.longitude,
-              ),
-            ));
-          });
-          // 現在地にカメラを移動
-          await mapController.animateCamera(
-            CameraUpdate.newCameraPosition(
-              CameraPosition(
-                target: LatLng(position.latitude, position.longitude),
-                zoom: 16.0,
-              ),
-            ),
-          );
-        });
+        markers.add(Marker(
+          markerId: const MarkerId('current_location'),
+          position: LatLng(
+            position.latitude,
+            position.longitude,
+          ),
+        ));
+      });
+      // 現在地にカメラを移動
+      await mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 16.0,
+          ),
+        ),
+      );
+    });
+  }
+
+  Future<void> _showNotification() async {
+    var androidChannelSpecifics = AndroidNotificationDetails(
+      'CHANNEL_ID',
+      'CHANNEL_NAME',
+      channelDescription: "CHANNEL_DESCRIPTION",
+      importance: Importance.max,
+      priority: Priority.high,
+      playSound: false,
+      timeoutAfter: 5000,
+      styleInformation: DefaultStyleInformation(true, true),
+    );
+
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0, // Notification ID
+      'お知らせ', // Notification Title
+      '～について', // Notification Body, set as null to remove the body
+      platformChannelSpecifics,
+      payload: 'New Payload', // Notification Payload
+    );
   }
 
   Future<void> _requestPermission() async {
